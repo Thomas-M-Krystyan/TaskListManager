d43458e8911e75c671751c79a7af2a593c9ed518
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2025-02-01 23:15:00 +0100

    Add new interface for custom TaskManager which will be used only by console application

---
+++ csharp/TaskList/Presentation/TaskList.ConsoleApp/Managers/Interfaces/IConsoleTaskManager.cs
@@ -0,0 +1,14 @@
+﻿using TaskList.Logic.Managers.Interfaces;
+
+namespace TaskList.ConsoleApp.Managers.Interfaces
+{
+    /// <summary>
+    ///   <inheritdoc cref="ITaskManager"/>
+    /// </summary>
+    /// <remarks>
+    ///   The console-specific implementation.
+    /// </remarks>
+    internal interface IConsoleTaskManager : ITaskManager
+    {
+    }
+}
