da44757c01ab9ed9c0476f5dfe2fa8c49fc2428e
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2025-02-01 23:34:06 +0100

    Introduce new service generating consequent IDs

---
+++ csharp/TaskList/Core/Application/TaskList.Logic/Helpers/CounterRegister.cs
@@ -0,0 +1,32 @@
+﻿using TaskList.Logic.Helpers.Interfaces;
+
+namespace TaskList.Logic.Helpers
+{
+    /// <inheritdoc cref="ICounterRegister"/>
+    public sealed class CounterRegister : ICounterRegister
+    {
+        private static readonly object _taskPadlock = new();
+
+        private static long _lastTaskId = 0;
+
+        /// <inheritdoc cref="ICounterRegister.GetNextTaskId()"/>
+        public long GetNextTaskId()
+        {
+            lock (_taskPadlock)
+            {
+                return ++_lastTaskId;
+            }
+        }
+
+        /// <summary>
+        /// Resets this shared counter.
+        /// </summary>
+        public static void Reset()
+        {
+            lock (_taskPadlock)
+            {
+                _lastTaskId = 0;
+            }
+        }
+    }
+}
---
+++ csharp/TaskList/Core/Application/TaskList.Logic/Helpers/Interfaces/ICounterRegister.cs
@@ -0,0 +1,18 @@
+﻿using TaskList.Domain.Models;
+
+namespace TaskList.Logic.Helpers.Interfaces
+{
+    /// <summary>
+    /// Controls the generation of unique IDs.
+    /// </summary>
+    public interface ICounterRegister
+    {
+        /// <summary>
+        /// Generates ID for the new <see cref="TaskItem"/>.
+        /// </summary>
+        /// <returns>
+        ///   The new, unique ID.
+        /// </returns>
+        public long GetNextTaskId();
+    }
+}
