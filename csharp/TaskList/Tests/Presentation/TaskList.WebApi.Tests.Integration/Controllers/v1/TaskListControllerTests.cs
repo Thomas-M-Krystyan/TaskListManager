7075ed7383c627cfe6494b5cad930395aab92543
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2025-02-02 02:40:46 +0100

    Add integration tests covering AddProjectAsync method from API controller

--- csharp/TaskList/Tests/Presentation/TaskList.WebApi.Tests.Integration/Controllers/v1/TaskListControllerTests.cs
+++ csharp/TaskList/Tests/Presentation/TaskList.WebApi.Tests.Integration/Controllers/v1/TaskListControllerTests.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using TaskList.Logic.Helpers;
-using TaskList.Logic.Helpers.Interfaces;
+using TaskList.Logic.Managers;
 using TaskList.Logic.Responses;
 using TaskList.WebApi.Controllers.v1;
 using TaskList.WebApi.Managers;
@@ -13,9 +13,15 @@ namespace TaskList.WebApi.Tests.Integration.Controllers.v1
     [TestFixture]
     public sealed class TaskListControllerTests
     {
+        #region Data
+        private const string ProjectName = "ProjectName 1";
+        #endregion
+
         [SetUp]
-        public void Setup()
+        public void SetUp()
         {
+            CounterRegister.Reset();
+            ConcurrentTaskManager.Reset();
         }
 
         #region DisplayTaskListAsync()
@@ -53,6 +59,51 @@ namespace TaskList.WebApi.Tests.Integration.Controllers.v1
 
             // Act
             ObjectResult result = (ObjectResult)await controller.DisplayTaskListAsync();
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(result.StatusCode, Is.EqualTo(StatusCodes.Status400BadRequest));
+                Assert.That(result.Value, Is.EqualTo($"Operation failed: {errorMessage}."));
+            });
+        }
+        #endregion
+
+        #region DisplayTaskListAsync()
+        [Test]
+        public async Task AddProjectAsync_HappyPath_IntegrationTest()
+        {
+            // Arrange
+            WebApiTaskManager taskManager = new(new CounterRegister());
+            TaskListController controller = new(taskManager);
+
+            // Act
+            ObjectResult result = (ObjectResult)await controller.AddProjectAsync(ProjectName);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(result.StatusCode, Is.EqualTo(StatusCodes.Status200OK));
+                Assert.That(result.Value, Is.EqualTo("Operation succeeded: The project with name \"ProjectName 1\" was created."));
+            });
+        }
+
+        [Test]
+        public async Task AddProjectAsync_RainyPath_IntegrationTest()
+        {
+            // Arrange
+            const string errorMessage = "AddProjectAsync failed.";
+
+            Mock<IWebApiTaskManager> taskManagerMock = new(MockBehavior.Strict);
+
+            _ = taskManagerMock
+                .Setup(mock => mock.AddProject(ProjectName))
+                .Returns(CommandResponse.Failure(errorMessage));
+
+            TaskListController controller = new(taskManagerMock.Object);
+
+            // Act
+            ObjectResult result = (ObjectResult)await controller.AddProjectAsync(ProjectName);
 
             // Assert
             Assert.Multiple(() =>
