60ceeb51ae633a5648948309109342a44ad4a85e
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2025-02-03 00:08:06 +0100

    Add simple unit tests covering the newly introduced method

--- csharp/TaskList/Tests/Presentation/TaskList.WebApi.Tests.Unit/Managers/WebApiTaskManagerTests.cs
+++ csharp/TaskList/Tests/Presentation/TaskList.WebApi.Tests.Unit/Managers/WebApiTaskManagerTests.cs
@@ -179,5 +179,68 @@ namespace TaskList.WebApi.Tests.Unit.Managers
             });
         }
         #endregion
+
+        #region DisplayTasksByDeadline()
+        [Test]
+        public void DisplayTasksByDeadline_TaskList_Empty_ReturnsSuccess()
+        {
+            // Arrange
+            WebApiTaskManager taskManager = new(_counterMock.Object);
+
+            // Act
+            CommandResponse response = taskManager.DisplayTasksByDeadline();
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(response.IsSuccess, Is.True);
+                Assert.That(response.Content, Is.EqualTo(EmptyJson));
+            });
+        }
+
+        [Test]
+        public void DisplayTasksByDeadline_TaskList_Filled_Today_ReturnsSuccess()
+        {
+            // Arrange
+            _ = _counterMock
+                .Setup(counter => counter.GetNextProjectId())
+                .Returns(default(long));
+
+            _ = _counterMock
+                .Setup(counter => counter.GetNextTaskId())
+                .Returns(TaskId);
+
+            WebApiTaskManager taskManager = new(_counterMock.Object);
+
+            DateOnly today = DateOnly.FromDateTime(DateTime.Today);
+            _ = taskManager.AddProject(ProjectName);
+            _ = taskManager.AddTask(ProjectName, TaskName);
+            _ = taskManager.SetDeadline(TaskId, today);
+
+            // Act
+            CommandResponse response = taskManager.DisplayTasksByDeadline();
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                _counterMock.Verify(mock => mock.GetNextProjectId(), Times.Once);
+                _counterMock.Verify(mock => mock.GetNextTaskId(), Times.Once);
+
+                string expectedJson =
+                    $"{{\"{TaskId}\":" +
+                      $"{{" +
+                        $"\"Id\":{TaskId}," +
+                        $"\"ProjectName\":\"{ProjectName}\"," +
+                        $"\"Name\":\"{TaskName}\"," +
+                        $"\"IsDone\":false," +
+                        $"\"Deadline\":\"{today:yyyy-MM-dd}\"" +
+                      $"}}" +
+                    $"}}";
+
+                Assert.That(response.IsSuccess, Is.True);
+                Assert.That(response.Content, Is.EqualTo(expectedJson));
+            });
+        }
+        #endregion
     }
 }
