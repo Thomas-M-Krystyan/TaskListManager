1a44bf467a6801829384f25fe637ac0ad5d6b73d
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2025-02-02 01:23:57 +0100

    Simple implementation of DisplayTaskList method returning serialized JSON as API response

---
+++ csharp/TaskList/Presentation/TaskList.WebApi/Managers/WebApiTaskManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+using TaskList.Logic.Helpers.Interfaces;
+using TaskList.Logic.Managers;
+using TaskList.Logic.Managers.Interfaces;
+using TaskList.Logic.Responses;
+using TaskList.WebApi.Managers.Interfaces;
+
+namespace TaskList.WebApi.Managers
+{
+    /// <inheritdoc cref="IWebApiTaskManager"/>
+    /// <seealso cref="ITaskManager"/>
+    internal sealed class WebApiTaskManager : ConcurrentTaskManager, IWebApiTaskManager
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WebApiTaskManager"/> class.
+        /// </summary>
+        public WebApiTaskManager(ICounterRegister counter) : base(counter)
+        {
+        }
+
+        /// <inheritdoc cref="ITaskManager.DisplayTaskList()"/>
+        public override CommandResponse DisplayTaskList()
+        {
+            try
+            {
+                return CommandResponse.Success(
+                    JsonSerializer.Serialize(GetTaskList()), true);
+            }
+            catch (Exception exception)
+            {
+                return CommandResponse.Failure(exception);
+            }
+        }
+    }
+}
