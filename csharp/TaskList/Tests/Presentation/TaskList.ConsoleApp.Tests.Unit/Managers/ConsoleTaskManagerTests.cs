6f728db0bba00434cd2fc348b3244cde7b4ef8df
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2025-02-02 22:21:56 +0100

    Add missing ConsoleTaskManager tests

--- csharp/TaskList/Tests/Presentation/TaskList.ConsoleApp.Tests.Unit/Managers/ConsoleTaskManagerTests.cs
+++ csharp/TaskList/Tests/Presentation/TaskList.ConsoleApp.Tests.Unit/Managers/ConsoleTaskManagerTests.cs
@@ -24,6 +24,23 @@ namespace TaskList.ConsoleApp.Tests.Unit.Managers
 
         #region DisplayAllTasks()
         [Test]
+        public void DisplayAllTasks_TaskList_Empty_ReturnsSuccess()
+        {
+            // Arrange
+            ConsoleTaskManager taskManager = new(new StringBuilderProxy(), _counterMock.Object);
+
+            // Act
+            CommandResponse result = taskManager.DisplayAllTasks();
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(result.IsSuccess, Is.True);
+                Assert.That(result.Content, Is.Empty);
+            });
+        }
+
+        [Test]
         public void DisplayAllTasks_TaskList_Filled_ReturnsSuccess()
         {
             // Arrange
@@ -63,23 +80,6 @@ namespace TaskList.ConsoleApp.Tests.Unit.Managers
         }
 
         [Test]
-        public void DisplayAllTasks_TaskList_Empty_ReturnsSuccess()
-        {
-            // Arrange
-            ConsoleTaskManager taskManager = new(new StringBuilderProxy(), _counterMock.Object);
-
-            // Act
-            CommandResponse result = taskManager.DisplayAllTasks();
-
-            // Assert
-            Assert.Multiple(() =>
-            {
-                Assert.That(result.IsSuccess, Is.True);
-                Assert.That(result.Content, Is.Empty);
-            });
-        }
-
-        [Test]
         public void DisplayAllTasks_StringBuilder_ThrowingException_ReturnsFailure()
         {
             // Arrange
@@ -97,6 +97,130 @@ namespace TaskList.ConsoleApp.Tests.Unit.Managers
 
             // Act
             CommandResponse result = taskManager.DisplayAllTasks();
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                _stringBuilderMock.Verify(mock => mock.Clear(), Times.Once);
+                _counterMock.Verify(mock => mock.GetNextTaskId(), Times.Never);
+
+                Assert.That(result.IsFailure, Is.True);
+                Assert.That(result.Content, Is.EqualTo($"Operation failed: {exceptionMessage}."));
+            });
+        }
+        #endregion
+
+        #region DisplayTodayTasks()
+        [Test]
+        public void DisplayTodayTasks_TaskList_Empty_ReturnsSuccess()
+        {
+            // Arrange
+            ConsoleTaskManager taskManager = new(new StringBuilderProxy(), _counterMock.Object);
+
+            // Act
+            CommandResponse result = taskManager.DisplayTodayTasks();
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(result.IsSuccess, Is.True);
+                Assert.That(result.Content, Is.Empty);
+            });
+        }
+
+        [Test]
+        public void DisplayTodayTasks_TaskList_Filled_NotToday_ReturnsSuccess()
+        {
+            // Arrange
+            const string projectName = "Project 1";
+            const string taskName = "Task 1";
+
+            _ = _counterMock
+                .Setup(mock => mock.GetNextProjectId())
+                .Returns(default(long));
+
+            _ = _counterMock
+                .Setup(mock => mock.GetNextTaskId())
+                .Returns(default(long));
+
+            ConsoleTaskManager taskManager = new(new StringBuilderProxy(), _counterMock.Object);
+
+            _ = taskManager.AddProject(projectName);
+            _ = taskManager.AddTask(projectName, taskName);
+
+            // Act
+            CommandResponse result = taskManager.DisplayTodayTasks();
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                _counterMock.Verify(mock => mock.GetNextProjectId(), Times.Once);
+                _counterMock.Verify(mock => mock.GetNextTaskId(), Times.Once);
+
+                Assert.That(result.IsSuccess, Is.True);
+                Assert.That(result.Content, Is.Empty);
+            });
+        }
+
+        [Test]
+        public void DisplayTodayTasks_TaskList_Filled_Today_ReturnsSuccess()
+        {
+            // Arrange
+            const string projectName = "Project 1";
+            const string taskName = "Task 1";
+            const long taskId = 1;
+
+            _ = _counterMock
+                .Setup(mock => mock.GetNextProjectId())
+                .Returns(default(long));
+
+            _ = _counterMock
+                .Setup(mock => mock.GetNextTaskId())
+                .Returns(taskId);
+
+            ConsoleTaskManager taskManager = new(new StringBuilderProxy(), _counterMock.Object);
+
+            _ = taskManager.AddProject(projectName);
+            _ = taskManager.AddTask(projectName, taskName);
+            _ = taskManager.SetDeadline(taskId, DateOnly.FromDateTime(DateTime.Today));
+
+            // Act
+            CommandResponse result = taskManager.DisplayTodayTasks();
+
+            // Assert
+            string expectedOutput =
+                $"{projectName}\r\n" +
+                $"    [ ] {taskId}: {taskName}\r\n" +
+                $"\r\n";
+
+            Assert.Multiple(() =>
+            {
+                _counterMock.Verify(mock => mock.GetNextProjectId(), Times.Once);
+                _counterMock.Verify(mock => mock.GetNextTaskId(), Times.Once);
+
+                Assert.That(result.IsSuccess, Is.True);
+                Assert.That(result.Content, Is.EqualTo(expectedOutput));
+            });
+        }
+
+        [Test]
+        public void DisplayTodayTasks_StringBuilder_ThrowingException_ReturnsFailure()
+        {
+            // Arrange
+            const string exceptionMessage = "Expected test exception";
+
+            _ = _stringBuilderMock
+                .Setup(mock => mock.Clear())
+                .Throws(new Exception(exceptionMessage));
+
+            _ = _counterMock
+                .Setup(mock => mock.GetNextTaskId())
+                .Returns(default(long));
+
+            ConsoleTaskManager taskManager = new(_stringBuilderMock.Object, _counterMock.Object);
+
+            // Act
+            CommandResponse result = taskManager.DisplayTodayTasks();
 
             // Assert
             Assert.Multiple(() =>
