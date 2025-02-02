17ecb0b6bde196c53b66e234a2262c0a6d1cce6c
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2025-02-02 02:12:21 +0100

    Introduce new project to be used for Web API integration tests

---
+++ csharp/TaskList/Tests/Presentation/TaskList.WebApi.Tests.Integration/Controllers/v1/TaskListController.cs
@@ -0,0 +1,16 @@
+namespace TaskList.WebApi.Tests.Integration.Controllers.v1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
